f9b5754fa818c0ea392f22ce4fd0526bc0c2c27e
adyatma-ardi <adyatmaardi@gmail.com>
2023-07-16 22:44:35 +0700

    konfigurasi tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './index.html',
+    './css/**/*.css',
+    './js/**/*.js',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
